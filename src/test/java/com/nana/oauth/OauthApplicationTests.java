db8afe059d998a150151853427b94d63899af4ba
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-09-01 15:18:26 +0000

    first commit

---
+++ src/test/java/com/nana/oauth/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nana.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
