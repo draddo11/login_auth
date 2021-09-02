a8f7a1e1cbf81d823538fda4395c3eb07edf9355
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-09-02 23:46:33 +0000

    added the routes for the admin and user roles

--- src/main/java/com/nana/oauth/controller/LoginController.java
+++ src/main/java/com/nana/oauth/controller/LoginController.java
@@ -1,8 +1,24 @@
 package com.nana.oauth.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 public class LoginController {
+    @RolesAllowed("USER")
+    @RequestMapping("/*")
+    public String getUser()
+    {
+        return "Welcome User";
+    }
+
+    @RolesAllowed({"USER","ADMIN"})
+    @RequestMapping("/admin")
+    public String getAdmin()
+    {
+        return "Welcome Admin";
+    }
 }
