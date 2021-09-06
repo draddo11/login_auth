5c0dc65cc7fe20bc719a0f484bb54e2d0026ca59
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-09-06 00:06:21 +0000

    oauth last part in restcomtroller donw

--- src/main/java/com/nana/oauth/controller/LoginController.java
+++ src/main/java/com/nana/oauth/controller/LoginController.java
@@ -6,11 +6,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
+import java.util.Map;
 
 @RestController
 public class LoginController {
@@ -37,7 +39,7 @@ public class LoginController {
     @RequestMapping("/*")
     public String getUserInfo(Principal user)
     {
-        StringBuffer userInfo= new StringBuffer;
+        StringBuffer userInfo= new StringBuffer();
 
         UsernamePasswordAuthenticationToken token = ((UsernamePasswordAuthenticationToken) user);
         if(token.isAuthenticated()){
@@ -54,12 +56,20 @@ public class LoginController {
         StringBuffer protectedInfo = new StringBuffer();
 
         OAuth2AuthenticationToken authToken = ((OAuth2AuthenticationToken) user);
-
         OAuth2AuthorizedClient authClient = this.authorizedClientService.loadAuthorizedClient(authToken.getAuthorizedClientRegistrationId(), authToken.getName());
+        if(authToken.isAuthenticated()){
 
+            Map<String,Object> userAttributes = ((DefaultOAuth2User) authToken.getPrincipal()).getAttributes();
+
+            String userToken = authClient.getAccessToken().getTokenValue();
+            protectedInfo.append("Welcome, " + userAttributes.get("name")+"<br><br>");
+            protectedInfo.append("e-mail: " + userAttributes.get("email")+"<br><br>");
+            protectedInfo.append("Access Token: " + userToken+"<br><br>");
+        }
+        else{
+            protectedInfo.append("NA");
+        }
         return protectedInfo;
-
     }
-
 
 }
