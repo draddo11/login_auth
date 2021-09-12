cfa42d7bd208a75ebc0d8d19e8843e9d1c06ab9b
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-09-12 23:43:25 +0000

    added allcontroller

--- src/test/java/com/nana/oauth/OauthApplicationTests.java
+++ src/test/java/com/nana/oauth/OauthApplicationTests.java
@@ -2,6 +2,7 @@ package com.nana.oauth;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.nana.oauth.controller.LoginController;
@@ -11,6 +12,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class OauthApplicationTests {
+
+    @Autowired
+    private LoginController controller;
 
     @Test
    public void contextLoads() {
