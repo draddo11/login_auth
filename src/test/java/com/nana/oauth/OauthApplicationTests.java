ca7f039aef9c125d02dd73a0511e02dba7c6f6ce
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-09-12 23:40:00 +0000

    adding more test

--- src/test/java/com/nana/oauth/OauthApplicationTests.java
+++ src/test/java/com/nana/oauth/OauthApplicationTests.java
@@ -3,7 +3,8 @@ package com.nana.oauth;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.nana.oauth.controller.LoginController;
 
 
 @ExtendWith(SpringExtension.class)
