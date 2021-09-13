f85541f4b9f51c4074b5a2366e455f6b05a61af0
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-09-13 22:12:53 +0000

    added the mock mvc private class

--- src/test/java/MockMvcTests.java
+++ src/test/java/MockMvcTests.java
@@ -1,11 +1,19 @@
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 
 public class MockMvcTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
 }
