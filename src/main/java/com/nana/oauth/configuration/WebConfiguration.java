a2a75a39bf2271ed73059a28b6583632c61c51a8
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-09-15 22:40:43 +0000

    adding the configuration

--- src/main/java/com/nana/oauth/configuration/WebConfiguration.java
+++ src/main/java/com/nana/oauth/configuration/WebConfiguration.java
@@ -1,4 +1,14 @@
 package com.nana.oauth.configuration;
 
-public class WebConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
 }
