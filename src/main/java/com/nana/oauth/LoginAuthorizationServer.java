a3627d69a52511491a8d47437714f8755df8ec91
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-09-23 21:39:36 +0000

    added the package of server configurerer

--- src/main/java/com/nana/oauth/LoginAuthorizationServer.java
+++ src/main/java/com/nana/oauth/LoginAuthorizationServer.java
@@ -3,5 +3,6 @@ package com.nana.oauth;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class LoginAuthorizationServer {
+@EnableAuthorizationServer
+public class LoginAuthorizationServer extends AuthorizationServerConfigurerAdapter {
 }
