c6e77fc177e36e1ae36f3490e725beea0c890da7
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-09-28 23:32:23 +0000

    cpmmenting authserver

--- src/main/java/com/nana/oauth/LoginAuthorizationServer.java
+++ src/main/java/com/nana/oauth/LoginAuthorizationServer.java
@@ -3,11 +3,11 @@ package com.nana.oauth;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAuthorizationServer
-public class LoginAuthorizationServer extends AuthorizationServerConfigurerAdapter {
-    @Value("${security.jwt.client-id}")
-    private String clientId;
-
-    @Value("${security.jwt.client-secret}")
-    private String clientSecret;
-}
+//@EnableAuthorizationServer
+//public class LoginAuthorizationServer extends AuthorizationServerConfigurerAdapter {
+//    @Value("${security.jwt.client-id}")
+//    private String clientId;
+//
+//    @Value("${security.jwt.client-secret}")
+//    private String clientSecret;
+//}
